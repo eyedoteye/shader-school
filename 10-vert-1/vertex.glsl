12498a2362ff9fa325a728875772098a01698add
sigmasleep <sarkar.krishnan@gmail.com>
2017-02-21 20:21:10 -0500

    Finish Rotation Problem

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -7,6 +7,7 @@ attribute vec2 position;
 void main() {
 
   //TODO: rotate position by theta radians about the origin
-
-  gl_Position = vec4(position, 0, 1.0);
+  mat2 rotationMatrix = mat2(cos(theta), -sin(theta),
+  			     sin(theta), cos(theta));
+  gl_Position = vec4(position * rotationMatrix, 0, 1.0);
 }
