44a688df5bd061adc48a9f5fa2c296188c21c253
sigmasleep <sarkar.krishnan@gmail.com>
2017-02-23 18:35:15 -0500

    Complete Geometry Problems

--- 15-geom-4/reflect.glsl
+++ 15-geom-4/reflect.glsl
@@ -1,10 +1,18 @@
+vec3 reflectPoint(vec3 p, vec3 n) {
+  return p - 2.0 * dot(n, p) * n / dot(n, n);
+}
+
 highp mat4 reflection(highp vec3 n) {
 
-  //TODO: Return a matrix that reflects all points about the plane passing through the origin with normal n
+  n = normalize(n);
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
+  vec3 col1 = reflect(vec3(1.0, 0.0, 0.0), n);
+  vec3 col2 = reflect(vec3(0.0, 1.0, 0.0), n);
+  vec3 col3 = reflect(vec3(0.0, 0.0, 1.0), n);
+
+  return mat4(col1.x, col1.y, col1.z, 0,
+              col2.x, col2.y, col2.z, 0, 
+              col3.x, col3.y, col3.z, 0,
               0, 0, 0, 1);
 }
 
--- 16-geom-5/rotate.glsl
+++ 16-geom-5/rotate.glsl
@@ -1,10 +1,22 @@
+vec3 rotatePoint(vec3 p, vec3 n, float theta) {
+  return (
+    p * cos(theta) + cross(n, p) *
+    sin(theta) + n * dot(p, n) *
+    (1.0 - cos(theta))
+  );
+}
+
 highp mat4 rotation(highp vec3 n, highp float theta) {
 
-  //TODO: Using Rodrigues' formula, find a matrix which performs a rotation about the axis n by theta radians
+  n = normalize(n);
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
+  vec3 col1 = rotatePoint(vec3(1.0, 0.0, 0.0), n, theta);
+  vec3 col2 = rotatePoint(vec3(0.0, 1.0, 0.0), n, theta);
+  vec3 col3 = rotatePoint(vec3(0.0, 0.0, 1.0), n, theta);
+
+  return mat4(col1.x, col1.y, col1.z, 0,
+              col2.x, col2.y, col2.z, 0, 
+              col3.x, col3.y, col3.z, 0,
               0, 0, 0, 1);
 }
 
