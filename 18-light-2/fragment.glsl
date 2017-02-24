3ae1e833d2c0035bc0730a4ea43878be94d4a373
sigmasleep <sarkar.krishnan@gmail.com>
2017-02-24 02:16:07 -0500

    Completed First Two Light Problems

--- 18-light-2/fragment.glsl
+++ 18-light-2/fragment.glsl
@@ -12,6 +12,22 @@ uniform vec3 ambient;
 uniform vec3 diffuse;
 uniform vec3 lightDirection;
 
+varying vec3 normal_p;
+
+vec3 reflectedLight(
+  vec3 normal,
+  vec3 lightDirection,
+  vec3 ambient,
+  vec3 diffuse
+){
+  float brightness = dot(normal, lightDirection);
+  return ambient + diffuse * max(brightness, 0.0);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 color = reflectedLight(normal_p,
+          		      lightDirection,
+			      ambient,
+			      diffuse);
+  gl_FragColor = vec4(color,1);
 }
