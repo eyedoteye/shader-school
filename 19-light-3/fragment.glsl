2015491b0e6144e97915da902de4a3cbef389a53
sigmasleep <sarkar.krishnan@gmail.com>
2017-02-24 03:34:22 -0500

    Finish Phong Shading Problem

--- 19-light-3/fragment.glsl
+++ 19-light-3/fragment.glsl
@@ -5,6 +5,34 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 viewPosition;
+varying vec3 normal_p;
+
+float phongWeight(
+  vec3 surfaceNormal,
+  vec3 lightDirection,
+  vec3 eyeDirection,
+  float shininess
+) {
+  vec3 rlight = reflect(lightDirection, surfaceNormal);
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+  return pow(eyeLight, shininess);
+}
+
+float lambertWeight(
+  vec3 surfaceNormal,
+  vec3 lightDirection
+) {
+  return max(dot(surfaceNormal, lightDirection), 0.0);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 color = ambient +
+               diffuse * lambertWeight(normal_p,
+	                               lightDirection) +
+               specular * phongWeight(normal_p,
+	                              lightDirection,
+				      viewPosition,
+				      shininess);
+  gl_FragColor = vec4(color,1);
 }
--- 19-light-3/vertex.glsl
+++ 19-light-3/vertex.glsl
@@ -6,6 +6,27 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 viewPosition;
+varying vec3 normal_p;
+
+mat4 transpose(mat4 matrix) {
+  vec4 col0 = matrix[0];
+  vec4 col1 = matrix[1];
+  vec4 col2 = matrix[2];
+  vec4 col3 = matrix[3];
+
+  return mat4( col0.x, col1.x, col2.x, col3.x,
+               col0.y, col1.y, col2.y, col3.y,
+	       col0.z, col1.z, col2.z, col3.z,
+	       col0.w, col1.w, col2.w, col3.w );
+}
+
 void main() {
-  gl_Position = vec4(position, 1);
+  viewPosition = normalize((view * 
+                            model *
+			    vec4(position, 1)).xyz);
+  normal_p = normalize((transpose(inverseModel *
+                                  inverseView) *
+		        vec4(normal, 0)).xyz);
+  gl_Position = projection * view * model * vec4(position, 1);
 }
