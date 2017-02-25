18c5f8caff6ff4fe0ee500cb0aba9acbd77712aa
sigmasleep <sarkar.krishnan@gmail.com>
2017-02-25 14:54:01 -0500

    Complete Game Of Life Problem

--- 24-gpgpu-1/life.glsl
+++ 24-gpgpu-1/life.glsl
@@ -7,12 +7,30 @@ float state(vec2 coord) {
   return texture2D(prevState, fract(coord / stateSize)).r;
 }
 
+int countNeighbors(vec2 coord) {
+  float neighbors = 0.0;
+  neighbors += state(coord + vec2(-1, -1));
+  neighbors += state(coord + vec2(-1, 0));
+  neighbors += state(coord + vec2(-1, 1));
+  neighbors += state(coord + vec2(0, -1));
+  neighbors += state(coord + vec2(0, 1));
+  neighbors += state(coord + vec2(1, -1));
+  neighbors += state(coord + vec2(1, 0));
+  neighbors += state(coord + vec2(1, 1));
+  return int(neighbors);
+}
+  
 void main() {
   vec2 coord = gl_FragCoord.xy;
-
-
-  //TODO: Compute the next state for the cell at coord
+ 
   float s = state(coord);
+  int neighbors = countNeighbors(coord);
+  
+  vec3 color = vec3(0); 
+  if(s == 0.0 && neighbors == 3
+  || s == 1.0 && (neighbors == 2 || neighbors == 3)){
+    color = vec3(1);
+  }
 
-  gl_FragColor = vec4(s,s,s, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
